1ab556d0176ac8361252570a709173eccd3089ce
chiayu lin <lin.1008@osu.edu>
2021-02-25 22:13:34 -0800

    Completed prep-javascript-for-loops.

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,14 @@
+// create your loops here.
+for (var i = 0; i <= 9; i++) {
+  console.log(i);
+}
+
+for (var j = 0; j < 10; j++) {
+  var m = j * 2;
+  console.log(m);
+}
+
+for (var k = 0; k < 100; k++) {
+  var n = 100 - k;
+  console.log('Time till explosion: ' + n + '!');
+}
